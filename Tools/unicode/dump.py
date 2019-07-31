4a13d0e92b9ab456b032e8fcccea5811a74c80c1
Greg Price <gnprice@gmail.com>
2019-07-31 00:09:00 -0700

    parametrize more

--- Tools/unicode/dump.py
+++ Tools/unicode/dump.py
@@ -3,29 +3,53 @@
 # and a "stick" was size 0x10, for the designers of ASCII.
 # Call size 0x100... a "line"?
 
+# Display choice we make: shape/size of a block.  Powers of 2.
+block_width_codepoints = 0x20  # min 2
+block_height_codepoints = 0x20  # min 4
+
+num_blocks = 0x10
+
+
 braille_base = ord('\u2800')  # BRAILLE PATTERN BLANK
 
-# Display coordinates of the single dot in glyph of chr(braille_base + (1 << i)).
+# Size of grid in one Braille character.
+braille_width = 2
+braille_height = 4
+
+# More facts about Braille in Unicode: the display coordinates
+# of the single dot in the glyph for `chr(braille_base + (1 << i))`.
 cell_offsets_yx = [(0,0), (1,0), (2,0), (0,1), (1,1), (2,1), (3,0), (3,1)]
 
-row_len = 0x10  # In codepoints
-cell_offsets = [y*row_len + x for y, x in cell_offsets_yx]
+
+block_width_cells = block_width_codepoints // braille_width
+block_height_cells = block_height_codepoints // braille_height
+
+cell_offsets = [y*block_width_codepoints + x for y, x in cell_offsets_yx]
+
+row_size_codepoints = braille_height * block_width_codepoints
+block_size_codepoints = block_height_codepoints * block_width_codepoints
+assert(block_size_codepoints == block_height_cells * row_size_codepoints)
+
+
+def braille_cell(base: int) -> str:
+    cell_codepoints = (base + offset for offset in cell_offsets)
+    cell_bits = sum(chr(c).isprintable() << i
+                    for i, c in enumerate(cell_codepoints))
+    return chr(braille_base + cell_bits)
+
 
 def braille_row(base: int) -> str:
-    s = ''
-    for cell_base in range(base, base + row_len, 2):
-        cell_codepoints = (cell_base + offset for offset in cell_offsets)
-        cell_bits = sum(chr(c).isprintable() << i
-                        for i, c in enumerate(cell_codepoints))
-        cell_repr = chr(braille_base + cell_bits)
-        s += cell_repr
-    return s
+    return ''.join(
+        braille_cell(cell_base)
+        for cell_base in range(base, base + block_width_codepoints,
+                               braille_width))
 
-for line in range(0x10):
-    line_base = line * 0x100
-    s = f'U+{line:02x}··:\n'
-    for row in range(0x100 // 4 // row_len):
-        s += ' '
-        s += braille_row(line_base + 4 * row_len * row)
-        s += '\n'
-    print(s)
+
+for block in range(num_blocks):
+    block_base = block * block_size_codepoints
+    print('U+{:04x}..U+{:04x}:'.format(
+        block_base, block_base + block_size_codepoints - 1))
+    print(''.join(
+        ' {}\n'.format(
+            braille_row(block_base + row_size_codepoints * row))
+        for row in range(block_height_cells)))
