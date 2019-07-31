849d12af05acba41697fa1cebe94f9a57f608fed
Greg Price <gnprice@gmail.com>
2019-07-31 02:26:10 -0700

    factor out stride logic

--- Tools/unicode/dump.py
+++ Tools/unicode/dump.py
@@ -2,34 +2,34 @@
 from typing import *
 
 
-class Grid:
+class Dims:
     def __init__(self, w: int, h: int) -> None:
         self.w = w
         self.h = h
         self.len = w * h
 
-    def __mul__(self, other: 'Grid') -> 'Grid':
-        return Grid(self.w * other.w,
+    def __mul__(self, other: 'Dims') -> 'Dims':
+        return Dims(self.w * other.w,
                     self.h * other.h)
 
-    def __floordiv__(self, other: 'Grid') -> 'Grid':
-        return Grid(self.w // other.w,
+    def __floordiv__(self, other: 'Dims') -> 'Dims':
+        return Dims(self.w // other.w,
                     self.h // other.h)
 
     def __repr__(self) -> str:
-        return f'Grid({self.w}, {self.h})'
+        return f'Dims({self.w}, {self.h})'
 
 
 # Display choice we make: shape/size of a block.  Powers of 2.
-block1_dots = Grid(0x20, 0x20)  # min 2, 4
-block2_block1 = Grid(2, 2)
+block1_dots = Dims(0x20, 0x20)  # min 2, 4
+block2_block1 = Dims(2, 2)
 num_block2 = 0x2
 
 
 braille_base = ord('\u2800')  # BRAILLE PATTERN BLANK
 
 # Size of grid in one Braille character.
-cell_dots = Grid(2, 4)
+cell_dots = Dims(2, 4)
 
 # More facts about Braille in Unicode: the display coordinates
 # of the single dot in the glyph for `chr(braille_base + (1 << i))`.
@@ -38,9 +38,29 @@ cell_offsets_yx = [(0,0), (1,0), (2,0), (0,1), (1,1), (2,1), (3,0), (3,1)]
 
 cell_offsets = [y*block1_dots.w + x for y, x in cell_offsets_yx]
 
+
+class Grid:
+    def __init__(self, outer: Dims, inner: Dims) -> None:
+        self.outer = outer
+        self.inner = inner
+        self.whole = outer * inner
+        self.row_len = self.whole.w * self.inner.h
+        assert(self.row_len == self.outer.w * self.inner.len)
+
+    def iterx(self, base: int) -> Iterable[int]:
+        return range(base, base + self.row_len, self.inner.len)
+
+    def itery(self, base: int) -> Iterable[int]:
+        return range(base, base + self.whole.len, self.row_len)
+
+
 block1_cells = block1_dots // cell_dots
 block2_cells = block2_block1 * block1_cells
 block2_dots = block2_block1 * block1_dots
+
+block1_grid = Grid(block1_cells, cell_dots)
+block2_grid = Grid(block2_block1, block1_dots)
+whole_grid = Grid(Dims(1, num_block2), block2_dots)
 
 
 def u_plus(codepoint: int) -> str:
@@ -60,47 +80,31 @@ def show_block10(base: int) -> str:
         for cell_base in range(base, base + block1_dots.w, cell_dots.w))
 
 
-def block20_block10_bases(base: int) -> Iterable[int]:
-    return range(base, base + block2_block1.w * block1_dots.len, block1_dots.len)
-
-
 def show_block20(base: int) -> str:
     return ' '.join(
-        show_block10(block10_base)
-        for block10_base in block20_block10_bases(base))
-
-
-def block21_block20_bases(base: int) -> Iterable[int]:
-    return range(base, base + block1_dots.len, block1_cells.w * cell_dots.len)
+        show_block10(inner_base)
+        for inner_base in block2_grid.iterx(base))
 
 
 def show_block21(base: int) -> str:
     heading = ' {}\n'.format(
-        ' '.join('{:{}}'.format(u_plus(block10_base),
+        ' '.join('{:{}}'.format(u_plus(inner_base),
                                 block1_cells.w)
-                 for block10_base in block20_block10_bases(base)))
+                 for inner_base in block2_grid.iterx(base)))
     return heading \
-        + ''.join(' {}\n'.format(show_block20(block20_base))
-                  for block20_base in block21_block20_bases(base))
-
-
-def block2_block21_bases(base: int) -> Iterable[int]:
-    return range(base, base + block2_dots.len, block2_block1.w * block1_dots.len)
+        + ''.join(' {}\n'.format(show_block20(inner_base))
+                  for inner_base in block1_grid.itery(base))
 
 
 def show_block2(base: int) -> str:
-    return '\n'.join(show_block21(block21_base)
-                     for block21_base in block2_block21_bases(base)) \
+    return '\n'.join(show_block21(inner_base)
+                     for inner_base in block2_grid.itery(base)) \
         + '\n'
 
 
-def all_block2_bases(base: int) -> Iterable[int]:
-    return range(base, base + num_block2 * block2_dots.len, block2_dots.len)
-
-
 def show_all(base: int) -> str:
-    return '\n'.join(show_block2(block2_base)
-                     for block2_base in all_block2_bases(base)) \
+    return '\n'.join(show_block2(inner_base)
+                     for inner_base in whole_grid.itery(base))
 
 
 print(show_all(0))
