b0d435e33fbc04b209a60118ee1b58dcce2aa46d
Greg Price <gnprice@gmail.com>
2019-07-31 01:18:25 -0700

    Make more uniform -- and fix a bug!  Note x and y coeffs in grid_row.

--- Tools/unicode/dump.py
+++ Tools/unicode/dump.py
@@ -1,43 +1,56 @@
 
-# A "plane" is size 0x10000;
-# and a "stick" was size 0x10, for the designers of ASCII.
-# Call size 0x100... a "line"?
+
+class Grid:
+    def __init__(self, w: int, h: int) -> None:
+        self.w = w
+        self.h = h
+        self.len = w * h
+
+    def __mul__(self, other: 'Grid') -> 'Grid':
+        return Grid(self.w * other.w,
+                    self.h * other.h)
+
+    def __floordiv__(self, other: 'Grid') -> 'Grid':
+        return Grid(self.w // other.w,
+                    self.h // other.h)
+
+    def __repr__(self) -> str:
+        return f'Grid({self.w}, {self.h})'
+
 
 # Display choice we make: shape/size of a block.  Powers of 2.
-block_width_codepoints = 0x20  # min 2
-block_height_codepoints = 0x20  # min 4
-
-grid_width_blocks = 0x4
-grid_height_blocks = 0x4
-
-num_grids = 0x1
+block1_runes = Grid(0x20, 0x20)  # min 2, 4
+block2_block1 = Grid(2, 2)
+num_block2 = 0x2
 
 
 braille_base = ord('\u2800')  # BRAILLE PATTERN BLANK
 
 # Size of grid in one Braille character.
-braille_width = 2
-braille_height = 4
+braille_dots = Grid(2, 4)
 
 # More facts about Braille in Unicode: the display coordinates
 # of the single dot in the glyph for `chr(braille_base + (1 << i))`.
 cell_offsets_yx = [(0,0), (1,0), (2,0), (0,1), (1,1), (2,1), (3,0), (3,1)]
 
 
-cell_offsets = [y*block_width_codepoints + x for y, x in cell_offsets_yx]
+cell_offsets = [y*block1_runes.w + x for y, x in cell_offsets_yx]
 
-block_width_cells = block_width_codepoints // braille_width
-block_height_cells = block_height_codepoints // braille_height
-
-row_size_codepoints = braille_height * block_width_codepoints
-block_size_codepoints = block_height_codepoints * block_width_codepoints
-assert(block_size_codepoints == block_height_cells * row_size_codepoints)
+block1_cells = block1_runes // braille_dots
 
 
-gridline_size_codepoints = grid_width_blocks * row_size_codepoints
-gridrow_size_codepoints = grid_width_blocks * block_size_codepoints
-assert(gridrow_size_codepoints == block_height_cells * gridline_size_codepoints)
-grid_size_codepoints = grid_height_blocks * gridrow_size_codepoints
+row_size_codepoints = braille_dots.h * block1_runes.w
+block_size_codepoints = block1_runes.len
+assert(block_size_codepoints == block1_cells.h * row_size_codepoints)
+
+
+block2_cells = block2_block1 * block1_cells
+block2_runes = block2_block1 * block1_runes
+
+gridline_size_codepoints = block2_block1.w * row_size_codepoints
+gridrow_size_codepoints = block2_block1.w * block1_runes.len
+assert(gridrow_size_codepoints == block1_cells.h * gridline_size_codepoints)
+grid_size_codepoints = block2_runes.len
 
 
 def u_plus(codepoint: int) -> str:
@@ -54,29 +67,28 @@ def braille_cell(base: int) -> str:
 def braille_row(base: int) -> str:
     return ''.join(
         braille_cell(cell_base)
-        for cell_base in range(base, base + block_width_codepoints,
-                               braille_width))
+        for cell_base in range(base, base + block1_runes.w, braille_dots.w))
 
 
 def grid_row(base: int) -> str:
     heading = ' {}\n'.format(
-        ' '.join('{:{}}'.format(u_plus(base + i * block_size_codepoints),
-                                block_width_cells)
-                 for i in range(grid_width_blocks)))
+        ' '.join('{:{}}'.format(u_plus(base + x * block1_runes.len),
+                                block1_cells.w)
+                 for x in range(block2_block1.w)))
     return heading \
         + ''.join(' {}\n'.format(
             ' '.join(braille_row(base
-                                 + y * row_size_codepoints
-                                 + x * gridline_size_codepoints)
-                     for x in range(grid_width_blocks)))
-                  for y in range(block_height_cells))
+                                 + x * block1_runes.len
+                                 + y * braille_dots.h * block1_runes.w)
+                     for x in range(block2_block1.w)))
+                  for y in range(block1_cells.h))
 
 
 def grid(base: int) -> str:
-    return '\n'.join(grid_row(base + i * gridrow_size_codepoints)
-                     for i in range(grid_height_blocks)) \
+    return '\n'.join(grid_row(base + i * block1_runes.h * block2_runes.w)
+                     for i in range(block2_block1.h)) \
         + '\n'
 
 
-for i_grid in range(num_grids):
-    print(grid(i_grid * grid_size_codepoints))
+for i_block2 in range(num_block2):
+    print(grid(i_block2 * block2_runes.len))
